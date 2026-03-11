fbb735db84bc2eeffe031d3c6a5bde7a822def62
renandiniz8 <158608712+renandiniz8@users.noreply.github.com>
2026-03-10 21:03:49 -0300

    Add Remedio class with setter methods

---
+++ ExercicioAula4/Remedio.java
@@ -0,0 +1,21 @@
+public class Remedio {
+    private String fabricante;
+    private int codigo;
+    private String atuacao;
+
+    public void setFabricante(String fabricante){
+        this.fabricante = fabricante;
+        //System.out.println(fabricante);
+    }
+
+    public void setCodigo(int codigo){
+        this.codigo = codigo;
+        //System.out.println(codigo);
+    }
+
+    public void setAtuacao(String atuacao){
+        this.atuacao = atuacao;
+        //System.out.println(atuacao);
+    }
+    
+}
